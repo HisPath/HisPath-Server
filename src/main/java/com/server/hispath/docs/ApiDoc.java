fe0088ec14568b43ff0fd69ec83764a1c32b65d2
gomster <gomster96@gmail.com>
2022-09-20 23:00:38 +0900

    docs[#2] : Category API Swagger 명세 작성

--- src/main/java/com/server/hispath/docs/ApiDoc.java
+++ src/main/java/com/server/hispath/docs/ApiDoc.java
@@ -9,5 +9,9 @@ public class ApiDoc {
     /* Activity 관련 API 명세 */
 
     /* Category 관련 API 명세 */
-
+    public static final String CATEGORY_READ = "단일 카테고리 조회";
+    public static final String CATEGORY_READ_ALL = "모든 카테고리 조회";
+    public static final String CATEGORY_CREATE = "카테고리 생성";
+    public static final String CATEGORY_UPDATE = "카테고리 수정";
+    public static final String CATEGORY_DELETE = "카테고리 삭제";
 }
