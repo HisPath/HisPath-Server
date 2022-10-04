71f0cb6641de81cafae3d0028bbda8c8b7453339
lukehongg <lukehongg@handong.ac.kr>
2022-10-04 15:06:18 +0900

    공지&매니저 데이터 추가

--- src/main/java/com/server/hispath/manager/domain/Manager.java
+++ src/main/java/com/server/hispath/manager/domain/Manager.java
@@ -4,9 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.*;
 
+import com.server.hispath.category.application.dto.CategoryCUDto;
+import com.server.hispath.category.domain.Category;
 import com.server.hispath.common.BaseEntity;
 import com.server.hispath.notice.domain.Notice;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
@@ -14,7 +18,9 @@ import org.hibernate.annotations.Where;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Where(clause = "deleted = false")
 @SQLDelete(sql = "UPDATE manager SET deleted = true Where id = ?")
 public class Manager extends BaseEntity {
@@ -28,6 +34,8 @@ public class Manager extends BaseEntity {
     private String department;
 
     private boolean approved;
+
+
 
 
 }
