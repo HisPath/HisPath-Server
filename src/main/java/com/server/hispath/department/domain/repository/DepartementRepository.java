b0fbe24e6ad3988972cfd25e4a5aa67378bb661a
gomster <gomster96@gmail.com>
2022-10-12 19:32:16 +0900

    feat[#28] : 전체 데이터 init해주는 controller 생성

--- src/main/java/com/server/hispath/department/domain/repository/DepartementRepository.java
+++ src/main/java/com/server/hispath/department/domain/repository/DepartementRepository.java
@@ -1,7 +1,10 @@
 package com.server.hispath.department.domain.repository;
 
+import java.util.Optional;
+
 import com.server.hispath.department.domain.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartementRepository extends JpaRepository<Department, Long> {
+    Department findByName(String name);
 }
--- src/main/java/com/server/hispath/major/domain/repository/MajorRepository.java
+++ src/main/java/com/server/hispath/major/domain/repository/MajorRepository.java
@@ -1,8 +1,12 @@
 package com.server.hispath.major.domain.repository;
 
+import java.util.Optional;
+
 import com.server.hispath.major.domain.Major;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MajorRepository extends JpaRepository<Major, Long> {
+
+    Major findByName(String name);
 }
