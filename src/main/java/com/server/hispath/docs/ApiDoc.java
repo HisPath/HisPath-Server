4a41eb404b0bb11b582362a5333cad04ed3459e2
gomster <gomster96@gmail.com>
2022-10-12 23:42:08 +0900

    docs[#31] : 마일리지 승인활동 관련 docs 수정

--- src/main/java/com/server/hispath/docs/ApiDoc.java
+++ src/main/java/com/server/hispath/docs/ApiDoc.java
@@ -16,8 +16,8 @@ public class ApiDoc {
     public static final String MILEAGES_CREATE = "단일 마일리지 다중 등록";
     public static final String MILEAGE_UPDATE = "마일리지 활동 수정";
     public static final String MILEAGE_DELETE = "마일리지 활동 삭제";
-    public static final String MILEAGE_READ_SEMESTER = "마일리지 학기별 조회";
-    public static final String MILEAGE_READ_ALL = "모든 마일리지 활동 조회";
+    public static final String MILEAGE_READ_SEMESTER = "승인된 마일리지 활동 학기별 조회";
+    public static final String MILEAGE_READ_ALL = "모든 승인된 마일리지 활동 조회";
     public static final String MILEAGE_REGISTER_STUDENTS = "마일리지 활동 참가 학생 엑셀 등록";
     public static final String ACTIVITY_STUDENT_DELETE = "마일리지 활동 학생 참가 정보 삭제";
     public static final String MILEAGE_READ = "단일 마일리지 활동 조회";
