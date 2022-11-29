5fce17874a1cca5f1f2feeb64e12ccf86a7f1479
gomster <gomster96@gmail.com>
2022-11-29 15:39:07 +0900

    hotfix : logging with file 구현

--- src/main/java/com/server/hispath/filters/ResponseWrapper.java
+++ src/main/java/com/server/hispath/filters/ResponseWrapper.java
@@ -1,11 +1,11 @@
-//package com.server.hispath.filters;
-//
-//import javax.servlet.http.HttpServletResponse;
-//
-//import org.springframework.web.util.ContentCachingResponseWrapper;
-//
-//public class ResponseWrapper extends ContentCachingResponseWrapper {
-//    public ResponseWrapper(HttpServletResponse response) {
-//        super(response);
-//    }
-//}
+package com.server.hispath.filters;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+public class ResponseWrapper extends ContentCachingResponseWrapper {
+    public ResponseWrapper(HttpServletResponse response) {
+        super(response);
+    }
+}
